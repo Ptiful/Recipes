f02ecdb021589ef06488b35606ecf95f1b6fa512
Paul Strazzulla <strazzu@gmail.com>
2024-04-23 13:35:44 +0200

    need to restart pc

--- Hello_Fresh_Scraper.py
+++ Hello_Fresh_Scraper.py
@@ -4,5 +4,8 @@ import requests
 main_url = "https://www.hellofresh.be/recipes?locale=fr-BE&redirectedFromAccountArea=true"
 
 r = requests.get(main_url).text
-soup = BeautifulSoup(r)
-print(soup.prettify())
+soup = BeautifulSoup(r, features="html.parser")
+most_popular = soup.find_all("h2")[0].get_text()
+href = soup.find_all("div", {"class": "web-gg4vpm"})
+# print(most_popular)
+print(href)
